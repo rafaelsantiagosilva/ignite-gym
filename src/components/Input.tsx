bfff80e421fe2ee284d5c8936a31469693b73510
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-01-14 11:11:12 -0300

    feat: profile screen

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,20 +3,22 @@ import { ComponentProps } from 'react';
 
 type Props = ComponentProps<typeof InputField>;
 
-export function Input({ ...rest }: Props) {
+export function Input({ readOnly, ...rest }: Props) {
 	return (
 		<GluestackInput
-			bg="$gray700"
 			height="$14"
-			px="$4"
 			borderWidth={0}
 			borderRadius="$md"
 			$focus={{
 				borderWidth: 1,
 				borderColor: '$green500',
 			}}
+			isReadOnly={readOnly}
+			opacity={readOnly ? 0.5 : 1}
 		>
 			<InputField
+				bg="$gray700"
+				px="$4"
 				color="$white"
 				fontFamily="$body"
 				placeholderTextColor="$gray300"
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,62 @@
-import { Center, Text } from '@gluestack-ui/themed';
+import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
+import { ScreenHeader } from '@/components/ScreenHeader';
+import { UserPhoto } from '@/components/UserPhoto';
+import { Center, Heading, Text, VStack } from '@gluestack-ui/themed';
+import { ScrollView, TouchableOpacity } from 'react-native';
 
 export function Profile() {
 	return (
-		<Center flex={1}>
-			<Text>Profile</Text>
-		</Center>
+		<VStack flex={1}>
+			<ScreenHeader title="Perfil" />
+
+			<ScrollView
+				style={{ marginTop: 24 }}
+				contentContainerStyle={{
+					paddingBottom: 36,
+				}}
+			>
+				<Center w="$full" px="$10">
+					<UserPhoto
+						size="2xl"
+						uri="https://github.com/rafaelsantiagosilva.png"
+						fallbackText="Rafael Santiago"
+					/>
+
+					<TouchableOpacity>
+						<Text color="$green500" fontFamily="$heading" fontSize="$md" mt="$2">
+							Alterar Foto
+						</Text>
+					</TouchableOpacity>
+
+					<Center mt="$8" gap="$4">
+						<Input placeholder="Nome" bg="$gray600" />
+						<Input
+							placeholder="E-mail"
+							value="rafaelsantiago@email.com"
+							bg="$gray600"
+							readOnly
+						/>
+					</Center>
+
+					<Heading
+						alignSelf="flex-start"
+						fontFamily="$heading"
+						color="$gray200"
+						fontSize="$md"
+						mt="$12"
+					>
+						Alterar senha
+					</Heading>
+
+					<Center w="$full" mt="$2" gap="$4">
+						<Input placeholder="Senha atual" bg="$gray600" secureTextEntry />
+						<Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+						<Input placeholder="Confirmar nova senha" bg="$gray600" secureTextEntry />
+						<Button title="Atualizar" />
+					</Center>
+				</Center>
+			</ScrollView>
+		</VStack>
 	);
 }
