a5e23d47ac2d2c4e46d99726e54f492d21fceec9
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-01-13 22:23:10 -0300

    feat: history exercises list component (SectionList)

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,30 @@
+import { Heading, HStack, VStack, Text } from '@gluestack-ui/themed';
+
+export function HistoryCard() {
+	return (
+		<HStack
+			bgColor="$gray600"
+			w="$full"
+			px="$5"
+			py="$4"
+			mb="$3"
+			bg="$gray600"
+			rounded="$md"
+			alignItems="center"
+			justifyContent="space-between"
+		>
+			<VStack>
+				<Heading color="$white" fontSize="$md">
+					Costas
+				</Heading>
+				<Text color="$gray100" fontSize={'$lg'} numberOfLines={1}>
+					Puxada Frontal
+				</Text>
+			</VStack>
+
+			<Text color="$gray300" fontSize="$md">
+				08:56
+			</Text>
+		</HStack>
+	);
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,56 @@
+import { HistoryCard } from '@/components/HistoryCard';
 import { ScreenHeader } from '@/components/ScreenHeader';
-import { VStack } from '@gluestack-ui/themed';
+import { Heading, Text, VStack } from '@gluestack-ui/themed';
+import { useState } from 'react';
+import { SectionList } from 'react-native';
 
 export function History() {
+	type SectionListData = {
+		title: string;
+		data: any[];
+	};
+
+	const [exercises, setExercises] = useState<SectionListData[]>([
+		{
+			title: '10.01.26',
+			data: ['1', '2'],
+		},
+		{
+			title: '11.01.26',
+			data: ['3'],
+		},
+	]);
+
 	return (
 		<VStack flex={1}>
-			<ScreenHeader title="Histórico" />
+			<ScreenHeader title="Histórico de Exercícios" />
+
+			<SectionList
+				sections={exercises}
+				keyExtractor={(item) => item}
+				renderItem={() => <HistoryCard />}
+				renderSectionHeader={({ section }) => (
+					<Heading
+						color="$gray200"
+						fontSize="$md"
+						mt="$10"
+						mb="$3"
+						fontFamily="$heading"
+					>
+						{section.title}
+					</Heading>
+				)}
+				style={{ paddingHorizontal: 24 }}
+				contentContainerStyle={
+					exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+				}
+				ListEmptyComponent={() => (
+					<Text color="$gray100" textAlign="center">
+						Não há exercícios registrados ainda.{'\n'} Vamos fazer exercícios hoje?
+					</Text>
+				)}
+				showsVerticalScrollIndicator={false}
+			/>
 		</VStack>
 	);
 }
